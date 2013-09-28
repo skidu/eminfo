e53c9387355a8f6b59b58cc587d1683a2102a084
Guangzheng Zhang <zhang.elinks@gmail.com>
2013-09-29 05:23:57 +0800

    remove <strong> from html_color; finish perl function format_phoutput_toxml();

--- bin/util.pl
+++ bin/util.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+# use Smart::Comments;
 
 my $action = shift;
 &help if !$action;
@@ -10,20 +11,55 @@ my $action = shift;
 &part_pstr_output(@ARGV)           if $action eq 'part_pstr_output';
 &format_pstr_output_toplain(@ARGV) if $action eq 'format_pstr_output_toplain';
 &format_pstr_output_toterm(@ARGV)  if $action eq 'format_pstr_output_toterm';
-&conv_phoutput_toxml(@ARGV)        if $action eq 'conv_phoutput_toxml';
+&format_phoutput_toxml(@ARGV)	   if $action eq 'format_phoutput_toxml';
 
-
-#### Sub Def
+### Sub Def
 
 # Help Stuff
 #
 sub help {
 print <<EOF
 Example:
+  part_pstr_output {1-6} {allof-plugin-output}}
+  format_pstr_output_toplain {output-contain-htmlcode}
+  format_pstr_output_toterm  {output-contain-htmlcode-htmlcolor}
+  format_phoutput_toxml {allof-plugin-output}  {allof-handler-output}
 
 EOF
 ;
 exit(1);
+}
+
+# Read each part of plugin output
+# Usage:        part_pstr_output part_num "${content}"  {mode}
+# Note:         part_num ~ [1-6]
+# Note:		mode ~ perl | print    default: print
+# Example:      part_pstr_output 2  "{level}:{type}:{title | summary | details: item1. ### item2. ### item3. ### }"
+# Example:      part_pstr_output 6  "{level}:{type}:{title | summary | details: item1. ### item2. ### item3. ### }"
+# 
+sub part_pstr_output {
+	my $part = shift;
+	my $content = shift;
+	my $mode = shift || 'print';
+	exit(1) if $part =~ /\D/;
+	exit(1) if ($part > 6 || $part < 1);
+	exit(1) if !defined $content;
+	# exit(1) if $content =~ m/\A\s*\Z/;
+	$content =~ m/{\s*(\w+)\s*}\s*:\s*{\s*(\w+)\s*}\s*:\s*{\s*(([^\|]+)(\|([^\|]+))?(\|([^\|]+))?)\s*}/i;
+	# print "1: $1\n2: $2\n3: $3\n4: $4\n5: $5\n6: $6\n7: $7\n8: $8\n\n";   # for debug
+	my $result = '';
+	if($1 && $part eq '1') { $result = $1; };
+	if($2 && $part eq '2') { $result = $2; };
+	if($3 && $part eq '3') { $result = $3; };
+	if($4 && $part eq '4') { $result = $4; };
+	if($6 && $part eq '5') { $result = $6; };
+	if($8 && $part eq '6') { $result = $8; };
+	$result =~ s/\A\s+//g if ($result);   # trim head \s
+	if ($mode eq 'perl'){
+		return ($result);
+	} elsif ($mode eq 'print'){
+		print $result;
+	}
 }
 
 # Trim HTML-TAG in plugin output (last filed)
@@ -50,8 +86,7 @@ sub format_pstr_output_toterm {
 	exit(1) if !defined $content;
 	exit(1) if $content =~ m/\A\s*\Z/;
 	$content =~ s/((<\s*font\s+color=(\w+)\s*>)\s*(.+?)\s*(<\s*\/font\s*>))/\n$3 ::: $4\n/ig;
-	# print "\nfirst===\n$content";  # for debug
-	# print "\nlast===\n";
+	### processed_content: $content
 	open my $fh, "<", \$content;
 	  while(<$fh>){
 		if (/\A(\w+)\s+:::\s+(.+)\Z/){
@@ -77,59 +112,98 @@ sub format_pstr_output_toterm {
 	close $fh;
 }
 
-# Read each part of plugin output
-# Usage:        part_pstr_output part_num "${content}"   
-# Note:         part_num ~ [1-6]
-# Example:      part_pstr_output 2  "{level}:{type}:{title | summary | details: item1. ### item2. ### item3. ### }"
-# Example:      part_pstr_output 6  "{level}:{type}:{title | summary | details: item1. ### item2. ### item3. ### }"
-# 
-sub part_pstr_output {
-	my $part = shift;
-	my $content = shift;
-	exit(1) if $part =~ /\D/;
-	exit(1) if ($part > 6 || $part < 1);
-	exit(1) if !defined $content;
-	exit(1) if $content =~ m/\A\s*\Z/;
-	$content =~ m/{\s*(\w+)\s*}\s*:\s*{\s*(\w+)\s*}\s*:\s*{\s*(([^\|]+)(\|([^\|]+))?(\|([^\|]+))?)\s*}/i;
-	# print "1: $1\n2: $2\n3: $3\n4: $4\n5: $5\n6: $6\n7: $7\n8: $8\n\n";   # for debug
-	if($1 && $part eq '1') { print $1; };
-	if($2 && $part eq '2') { print $2; };
-	if($3 && $part eq '3') { print $3; };
-	if($4 && $part eq '4') { print $4; };
-	if($6 && $part eq '5') { print $6; };
-	if($8 && $part eq '6') { print $8; };
-}
-
-
 # Convert plugin/handler output to xml-content
 # Note:		called by handler postlog
-# Note:  	deal with:   < > & ' " 
-# 		&amp; &
-# 		&lt;  <
-# 		&gt;  >
-# 		&quot; "
-# 		&apos; '
-# Example:      conv_phoutput_toxml  "{level}:{type}:{title | summary | details: item1. ### <font color=red> item2. &nbsp; </font> ### item3. ### }"  "123"
-# Example:	conv_phoutput_toxml  "{crit}:{file}:{ /etc/passwd /etc/services /etc/shadow }"  "auto handler result here"
+# Note:		process chars:  & < > ' "   =====>  &amp; &lt; &gt; &apos; &quot;
+# Example:      format_phoutput_toxml  "{level}:{type}:{title | summary | details: item1. ### <font color=red> item2. &nbsp; </font> ### item3. ### }"  "123"
+# Example:      format_phoutput_toxml  "{level}:{type}:{title}"  "123"
+# Example:	format_phoutput_toxml  "{crit}:{file}:{ /etc/passwd /etc/services /etc/shadow }"  "auto handler result here"
 #
-sub conv_phoutput_toxml {
-	my $debug = 1;
+sub format_phoutput_toxml {
 	my $poutput = shift;
 	my $houtput = shift;
-	exit(1) if !defined $poutput;
-	exit(1) if !defined $houtput;
-	print "poutput: $poutput\n" if $debug;
-	print "houtput: $houtput\n" if $debug;
-	my $level = &part_pstr_output(1,$poutput);
-	print "level=[$level]\n" if $debug;
-	my $type = &part_pstr_output(2,$poutput);
-	print "type=[$type]\n" if $debug;
-	my $title = &part_pstr_output(4,$poutput);
-	print "title=[$title]\n" if $debug;
-	my $summary = &part_pstr_output(5,$poutput);
-	print "summary=[$summary]\n" if $debug;
-	my $body = &part_pstr_output(6,$poutput);
-	print "body=[$body]\n" if $debug;
-	my @items = split /###/ , $body;
-	print "array item=[@items]\n" if $debug;
+	my $xml_result;
+	#exit(1) if !defined $poutput;
+	#exit(1) if !defined $houtput;
+	### poutput: $poutput
+	### houtput: $houtput
+	
+	$xml_result = "<eminfo_plugin_result>\n";
+
+	my $level = &part_pstr_output(1,$poutput,'perl') || '';
+	$xml_result .= "<level>$level</level\n";
+	### level: $level
+
+	my $type = &part_pstr_output(2,$poutput,'perl') || '';
+	$xml_result .= "<type>$type</type>\n";
+	### type: $type
+
+	$xml_result .= "<body>\n";
+
+	if ($type eq 'file'){
+		my $files = &part_pstr_output(3,$poutput,'perl') || '';
+		### files: $files
+		my @files = split /\s+/, $files;
+		### array_files: @files
+		for (@files) {
+			my $file_size = -s || 'unkn';
+			$xml_result .= "<file size=$file_size>$_</file>\n";
+		}
+	} elsif ($type eq 'str'){
+		my $title = &part_pstr_output(4,$poutput,'perl') || '';
+		### title: $title
+		$xml_result .= "<title>$title</title>\n";
+
+		my $summary = &part_pstr_output(5,$poutput,'perl') || '';
+		### summary: $summary
+		$xml_result .= "<summary>$summary</summary>\n";
+
+		my $body = &part_pstr_output(6,$poutput,'perl') || '';
+		### body: $body
+		my @lines = split /###/ , $body;
+		### array_lines: @lines
+		for (@lines) {
+			my ($color,$content);
+			# must whole-line matches     ~     <font color=(\w+)>content</font>
+			if (m/\A\s*(<\s*font\s+color=(\w+)\s*>)\s*(.+?)\s*(<\s*\/font\s*>)\s*\Z/) { 
+				($color,$content) = ($2,$3);
+				### color: $color
+				### content: $content
+			} else {
+				$content = $_;
+			}
+			$content =~ s/\A\s+//g;   # trim head \s
+			$content =~ s/[\r\n]//g;  # trim \r\n
+			$content =~ s/&nbsp;/ /g;  # replace html space
+			$content =~ s/\<[^\<\>]*\>//g; # trim html-tag
+			# replace unsupported chars
+			$content =~ s/&/&amp;/g;
+			$content =~ s/</&lt;/g;
+			$content =~ s/>/&gt;/g;
+			$content =~ s/"/&quot;/g;
+			$content =~ s/'/&apos;/g;
+			$xml_result .= sprintf("<line size=%d%s>$content</line>\n",length($content),($color)?" color=$color":"");
+		}
+	}
+	$xml_result .= "</body>\n";
+	$xml_result .= "</eminfo_plugin_result>\n";
+	
+	$xml_result .= "<eminfo_autohandle_result>\n";
+	my @hlines = split /###/, $houtput;
+	for (@hlines) {
+		s/\A\s+//g;   # trim head \s
+		s/[\r\n]//g;  # trim \r\n
+		s/&nbsp;/ /g;   # replace html space
+		s/\<[^\<\>]*\>//g;  # trim html-tag
+		# replace unsupported chars
+		s/&/&amp;/g;  
+		s/</&lt;/g;
+		s/>/&gt;/g;
+		s/"/&quot;/g;
+		s/'/&apos;/g;
+		$xml_result .= sprintf("<line size=%d>$_</line>\n",length);
+	}
+
+	$xml_result .= "</eminfo_autohandle_result>\n";
+	print $xml_result;
 }
